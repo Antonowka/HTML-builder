270d5246a82c598879e13659e08d3fa31794c177
Anton <102463034+Antonowka@users.noreply.github.com>
2022-11-06 19:12:17 +0100

    feat: added task solution 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,5 @@
+let fs = require('fs'); //import fs module
+let path = require('path');  //correct path
+
+let readStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8'); //create new ReadStream
+readStream.on('data', (data) => console.log(data.trim()));
