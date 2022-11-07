11fe34ce289542672e66a84d3e3718503874ae7f
Anton <102463034+Antonowka@users.noreply.github.com>
2022-11-07 23:29:28 +0100

    feat: added task solution 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,13 +1,18 @@
-let fs = require('fs'); //import fs module
-let path = require('path'); //correct path
+const fs = require('fs'); //import fs module
+const path = require('path'); //correct path
+
 
 fs.readdir(path.join(__dirname, 'secret-folder'), { // create folder path
   withFileTypes: true //  if the setting With file types is true or not specified, the function returns a list of file names.
-}, (err, files) => {
+}, (error, files) => {
   for (let file of files) {
-    fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stats) => {
+    fs.stat(path.join(__dirname, 'secret-folder', file.name), (error, stats) => {
       if (file.isFile()) {
-        console.log(`${file.name.split('.')[0]} - ${file.name.split('.')[1]} - ${stats.size / 1000} kB`);
+        const fileName = file.name.split('.')[0];
+        const fileExtension = file.name.split('.')[1];
+        const fileSize = stats.size / 1000; // b -> kB
+        const output = `${fileName} - ${fileExtension} - ${fileSize} kB`;
+        console.log(output);
       }
     });
   }
